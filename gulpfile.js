8db904b1799455871868fb223d53e5440faebc4b
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-11-08 14:12:59 +0100

    site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')
+// css
 var cleanCSS = require('gulp-clean-css')
+var postcss = require('gulp-postcss')
 var sourcemaps = require('gulp-sourcemaps')
+var concat = require('gulp-concat')
+
+// browser refresh
 var browserSync = require('browser-sync').create()
+
+// images
 var imagemin = require('gulp-imagemin')
+
+// github
 var ghpages = require('gh-pages')
 
-sass.compiler = require('node-sass')
 
-gulp.task("sass", function() {
-    return gulp.src("src/css/app.scss")
+
+gulp.task("css", function() {
+    return gulp.src([
+        "src/css/reset.css",
+        "src/css/typography.css",
+        "src/css/app.css"
+    ])
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(
+        postcss([
+            require("autoprefixer"),
+            require("postcss-preset-env")({
+                stage: 1,
+                browsers: ["IE 11", "last 2 versions"]
+            })
+        ])
+    )
+    .pipe(concat("app.css"))
+    .pipe(
+        cleanCSS({
+            compatibility: 'ie8'
+        })
+    )
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("dist"))
     .pipe(browserSync.stream())
@@ -42,7 +67,7 @@ gulp.task("watch", function() {
     });
 
     gulp.watch("src/*.html", gulp.series("html")).on('change', browserSync.reload)
-    gulp.watch("src/css/app.scss", gulp.series("sass"))
+    gulp.watch("src/css/*", gulp.series("css"))
     gulp.watch("src/fonts/*", gulp.series("fonts"))
     gulp.watch("src/img/*", gulp.series("images"))
 })
@@ -51,4 +76,4 @@ gulp.task("deploy", function () {
     ghpages.publish("dist")
 })
 
-gulp.task('default', gulp.series("html", "sass", "fonts", "images", "watch"))
+gulp.task('default', gulp.series("html", "css", "fonts", "images", "watch"))
